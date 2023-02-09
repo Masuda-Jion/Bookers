3b2976fc95f472c5f11e3e14885870da478b3376
Masuda-Jion <dgg2>
2023-02-09 14:16:47 +0000

    [add] rspec修正前

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,6 +1,4 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
     @book = Book.new
@@ -8,9 +6,24 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
@@ -20,10 +33,10 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def edit
-  end
+
 
   private
   #ストロングパラメータ
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books' => 'books#index', as: 'index_books'
-  post 'books' => 'books#create'
-  get 'books/show' => 'books#show', as: 'show_books'
-  get 'books/edit'
+  resources :books
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'book/:id' => 'books#show'
-  delete 'books/:id' => 'books#destroy' , as: 'destroy_book'
+
 end
